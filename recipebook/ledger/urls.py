9222d8c7613a25643a76e86824c853de5cfcad91
Ritsu- <biasonneilaldous@gmail.com>
2025-03-05 23:45:52 +0800

    applied clean coding practices

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import RecipeListView, RecipeDetailView
 
-app_name = 'ledger'
-
 urlpatterns = [
     path('recipes/list/', RecipeListView.as_view(), name="recipe_list"),
     path('recipe/<int:pk>/', RecipeDetailView.as_view(), name="recipe_detail"),
 ]
+
+app_name = 'ledger'
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from .models import Recipe
 
 class RecipeListView(ListView):
-   
+    """Displays the list of all recipes"""
     model = Recipe
-    template_name = 'recipe_list.html'
-
+    template_name = "recipe_list.html"
+    context_object_name = "recipes"
 
 class RecipeDetailView(DetailView):
-   
+    """Displays detailed information of a recipe."""
     model = Recipe
     template_name = "recipe_detail.html"
+    context_object_name = "recipe"
