792bf93bd6c15ed57b98ce3575cf91180d8164de
Ritsu- <biasonneilaldous@gmail.com>
2025-04-07 23:08:51 +0800

    did the bonus, added the .env file (which is gitignored)

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,13 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-caqu@dwoj=7unc@))&5#gdy^fy)s)y=qj02r%rjt6tc3rwl794'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
