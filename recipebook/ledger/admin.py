973b9579a5c2755c40f330a3048385ae24729163
Ritsu- <biasonneilaldous@gmail.com>
2025-04-07 15:00:16 +0800

    Modified the Recipe admin to contain the Recipe Images related to it

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,30 +1,29 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Ingredient, Recipe, RecipeIngredient, Profile
+from .models import Ingredient, Recipe, RecipeIngredient, Profile, RecipeImage
 
 
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
 
-
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
 
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
     list_display_links = ('name',)
 
-
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
     list_display_links = ('name',)
-    inlines = [RecipeIngredientInline]
-
+    inlines = [RecipeIngredientInline, RecipeImageInline]
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
     list_display = ('get_ingredient_name', 'quantity', 'get_recipe_name')
@@ -40,13 +39,11 @@ class RecipeIngredientAdmin(admin.ModelAdmin):
     def get_recipe_name(self, obj):
         return obj.recipe.name
 
-
 class CustomUserAdmin(BaseUserAdmin):
     inlines = (ProfileInline,)
     
     def __str__(self):
         return self.username
-
 
 
 admin.site.unregister(User)
